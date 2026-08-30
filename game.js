b92b24a8de66daa304148c6a3d7d6d377c0b9358
Pocket Racing <tomas34rus@gmail.com>
2026-08-31 00:23:13 +0300

    Create game.js

---
+++ game.js
@@ -0,0 +1,24 @@
+<canvas id="gameCanvas"></canvas>
+
+<div id="notifications"></div>
+
+<div id="hudName"></div>
+<div id="hudLevel"></div>
+<div id="hudCash"></div>
+<div id="hudBank"></div>
+<div id="hudJob"></div>
+
+<div id="hudHealth"></div>
+<div id="hudArmor"></div>
+
+<div id="hudHealthBar"></div>
+<div id="hudEnergyBar"></div>
+
+<div id="pauseScreen"></div>
+
+<button id="mobileUp">▲</button>
+<button id="mobileDown">▼</button>
+<button id="mobileLeft">◀</button>
+<button id="mobileRight">▶</button>
+
+<script src="game.js"></script>
